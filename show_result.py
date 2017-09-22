4ed908f4fbcc9b566637ad0cf569d72fb39b2bc9
Hung Nguyen <hungnguyen94@gmail.com>
2017-09-22 12:04:45 +0200

    Add stdin support for show_result.py
    Usage:
    
    \# python print_dimacs.py "412.3.....7.5.............43.4.98....59....2....2..9...6.78...384......1..7......" | minisat | python show_result.py

--- show_result.py
+++ show_result.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 from SudokuConverter import SudokuConverter
-import pycosat
 import argparse
-from ptpython.repl import embed
+import sys
 
 def main(args):
+    if args.sudoku == '':
+        args.sudoku = '0' * args.n**2
+
     if args.eval:
         arr = eval(args.sudoku)
     else:
         arr = list(args.sudoku.replace(args.fill_value, '0'))
     arr = [int(x) for x in arr]
-    #  solution = [int(x) for x in args.solution.split()[:-1]]
 
     sudoku = SudokuConverter(args.n, arr)
-    cnf = sudoku.convert_to_sat()[1:]
-    solution = pycosat.solve(cnf, verbose=True)
-    print(solution)
+    if args.pycosat:
+        import pycosat
+        cnf = sudoku.convert_to_sat()[1:]
+        solution = pycosat.solve(cnf, verbose=True)
+    else:
+        solution = [int(x) for x in args.solution.readline().split()[:-1]]
+
+    #  print(solution)
     sudoku.print_board()
     sudoku.enter_solution(solution)
     sudoku.print_board()
@@ -24,12 +30,13 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert a SAT assignment to Sudoku.')
-    #  parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-    parser.add_argument("sudoku", type=str)
+    parser.add_argument('solution', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument("--sudoku", type=str, default='')
     parser.add_argument("--eval", type=bool, default=False)
-    parser.add_argument("--solution", type=str)
+    #  parser.add_argument("--solution", type=str)
     parser.add_argument("--fill_value", type=str, default='.')
     parser.add_argument("-n", type=int, default=9)
+    parser.add_argument("--pycosat", type=bool, default=False)
     args = parser.parse_args()
     main(args)
 
